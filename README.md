0827e8de29830eff514a350fcb924818a737a103
Liam McArdle <mcardle.liam@gmail.com>
2018-12-05 23:27:48 -0800

    add basic rollup setup

---
+++ README.md
@@ -0,0 +1,14 @@
+# Curo Base
+
+- [X] Basic Rollup Build Setup
+- [ ] React
+- [ ] Typescript
+- [ ] Dev Server for Apps
+- [ ] Dev/Test/Documentation Server for Libraries
+- [ ] UI Unit Tests
+- [ ] JS Unit Tests
+- [ ] CSS Strategy
+- [ ] Mock Database
+- [ ] Env Configs (Dev, Test, Beta, Production)
+- [ ] Data Persistence (only apps, not libraries?)
+- [ ] String Management
---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import replace from 'rollup-plugin-replace';
+import rimraf from 'rimraf';
+
+rimraf.sync('dist');
+
+const pkg = require('./package.json');
+const { version } = pkg;
+
+export default {
+  input: 'src/scripts/app.js',
+  output: {
+    file: 'dist/scripts/app.js',
+    format: 'iife'
+  },
+  plugins: [
+    replace({
+      delimiters: ['{{', '}}'],
+      version
+    })
+  ]
+}
